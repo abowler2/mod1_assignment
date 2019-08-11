fd148d50617487180f355d8865f408af3c328ab0
abowler <abowler2@gmail.com>
2019-08-11 11:01:45 -0700

    * .gitignore: Initial commit
    * mod1_assignment.sln:
    * mod1_assignment.csproj: initial commit
    
    * Program.cs: added variables

---
+++ mod1_assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace mod1_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create variables for student and teacher
+            string firstName = "";
+            string lastName = "";
+            DateTime birthdate;
+            string addressLine1 = "";
+            string addressLine2 = "";
+            string city = "";
+            string state_province = "";
+            string country = "";
+
+            //create variables for program info
+            string programName = "";
+            string deptHead = "";
+            string degrees = "";
+
+            //create variables for degree info
+            string degreeName = "";
+            int creditsRequired = 0;
+
+            //create variables for course info
+            string courseName = "";
+            int credits = 0;
+            int durationWeeks = 0;
+            string teacher = "";
+
+        }
+    }
+}
